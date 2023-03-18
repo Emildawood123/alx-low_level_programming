78c97af87786a52cfb3d60ba40e605e6adb1592a
Your Name <you@example.com>
2023-03-18 13:47:03 -0700

    0

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - that to hundred without ascading
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 567; i < 790; i++)
+	{
+		putchar(i % 10 +'0');
+		putchar(i % 100 +'0');
+		putchar(i % 1000 +'0');
+	}
+
+}
