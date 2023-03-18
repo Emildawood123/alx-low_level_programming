ab917679d64ce450271a08d1e772ceb32d3c2adf
Your Name <you@example.com>
2023-03-18 07:12:07 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - this program print all lowercase alphabet
+ * Return: 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char alph[26] = "abcdefghijkmnopqrstuvwxyz";
+	int n;
+
+	for (n = 0; n < 26; n++)
+	{
+		putchar (alph[n]);
+	}
+	/* your code goes there */
+	return (0);
+}
