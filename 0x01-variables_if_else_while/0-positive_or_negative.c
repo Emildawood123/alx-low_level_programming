c3992f6f8bd912048d2fd104fa5d2aa87db2db0d
Your Name <you@example.com>
2023-03-17 17:57:00 -0700

    n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,25 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ * main - that application tell the number neg or pos
+ * Return: 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n == 0)
 	{
-	printf("%d is zero", n);
+	printf("%d is zero\n", n);
 	}
 	else if (n > 0)
 	{
-	printf("%d is positive", n);
+	printf("%d is positive\n", n);
 	}
 	else 
 	{
-	printf("%d is negative", n);
+	printf("%d is negative\n", n);
 	}
 	return (0);
 }
