5ee206689e754e8ccd62c49b7fc12a9cd459f977
Your Name <you@example.com>
2023-03-18 11:53:19 -0700

    kk

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,11 @@
 int main(void)
 {
 	int n;
-for (n = 0; n < 90; n++)
+for (n = 1; n < 90; n++)
+	if (n == 10 || n == 11)
+	{
+	continue;
+	}
 	{
 	if (n < 10)
 	{
@@ -22,6 +26,7 @@ for (n = 0; n < 90; n++)
 	if (n != 89)
 	{
 		putchar(',');
+		putchar(' ');
 	}
 	}
 	return (0);
