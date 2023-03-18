2224d64ec31240e7d2828e6e161bd7eac234c8a2
Your Name <you@example.com>
2023-03-17 17:27:41 -0700

    if or while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	char[50] cas;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n == 0)
+	{
+	cas = "zero"
+	}
+	else if (n > 0)
+	{
+	cas = "positive"
+	}
+	else 
+	{
+	cas = "negative"
+	}
+	printf("%d is %s", n, cas)
+	return (0);
+}
