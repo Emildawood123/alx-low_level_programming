df5e20d0d5f58e13659fc6749617854ccb112d92
Your Name <you@example.com>
2023-03-19 05:58:37 -0700

    2:36

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,21 @@ int main(void)
 {
 	int i;
 
-	for(i = 10; i < 790; i++)
+	for (i = 10; i < 790; i++)
 	{
 	if (i < 100 && i % 10 + '0' <= i / 10 + '0')
 	{
 	continue;
-	}	
-	else if (i % 10 + '0' <= (i / 10) % 10 + '0'|| (i / 10) % 10 + '0' <= i / 100 + '0')
+	}
+	else if (i % 10 + '0' <= (i / 10) % 10 + '0' || (i / 10) % 10 + '0' <= i / 100 + '0')
 	{
 	continue;
 	}
-	
-	else{
-	if (i < 100)
+	else
 	{
-	putchar('0');
-	}
-	putchar(i < 100 ? i / 10 + '0' : i / 100 + '0');
-	putchar(i < 100 ? i % 10 + '0' : (i / 10) % 10 + '0');
-	if (i >= 100)
-	{
+	putchar(i < 100 ? '0' : i / 100 + '0');
+	putchar(i < 100 ? i / 10 + '0' : (i / 10) % 10 + '0');
 	putchar(i % 10 + '0');
-	}
 	if (i != 789)
 	{
 	putchar(',');
@@ -36,5 +29,7 @@ int main(void)
 	}
 	}
 	}
+
+	putchar('\n');
 	return (0);
 }
