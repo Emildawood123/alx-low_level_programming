5b8fb6abaa373ef15a88c469bc179ba797211ca7
Your Name <you@example.com>
2023-03-21 09:35:15 -0700

    if lower is 0 else 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - return zero or one if c is lower
+ * @c: the chara
+ * Return: On success 1
+ */
+int _islower(int c)
+{
+if (c < 97)
+{
+	return (0);
+}
+else
+{
+	return (1);
+}
+
+}
