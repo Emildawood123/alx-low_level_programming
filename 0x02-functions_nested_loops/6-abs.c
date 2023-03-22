5b126da55b4e9ccc46c5b31099f32273d98c6b2c
Your Name <you@example.com>
2023-03-21 17:53:36 -0700

    absoulte numbers

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _abs - that return abs num
- * @ - that is num
+ * @c:  that is num
  * Return: On success
  */
 int _abs(int c)
 {
-	if(c < 0)
+	if (c < 0)
 	{
 	return (c - c - c);
 	}
