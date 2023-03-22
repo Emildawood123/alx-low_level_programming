20546b69586b4cdf8923b5dd9d7712e2dda216e1
Your Name <you@example.com>
2023-03-22 07:54:16 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - to add two numbers
+ * @a: this is first number
+ * @b: this is second number
+ * Return: On succes intgeration
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
