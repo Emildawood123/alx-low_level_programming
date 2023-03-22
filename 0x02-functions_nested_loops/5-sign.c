7958774f1a9c8ff723edcd56608a08d2b68407c3
Your Name <you@example.com>
2023-03-21 17:31:43 -0700

    sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - that return - or + or zero
+ * @n: this parameter to know is minus of right
+ * Return: On success 
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
