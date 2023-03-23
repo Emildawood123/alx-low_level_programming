a17020307ff9e2f36afbe01ac9a61a364e6b323e
Your Name <you@example.com>
2023-03-23 07:03:05 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - that check the chara is uppercasae
+ * @c: that is chara
+ * Return: On succes 1
+ */
+int _isupper(int c)
+{
+	if (c < 97)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
