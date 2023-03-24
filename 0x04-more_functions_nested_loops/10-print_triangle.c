26a8de3a1f0c23870ef8ffc71ff0619acae360a8
Your Name <you@example.com>
2023-03-24 07:27:15 -0700

    try 3

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,9 @@ void print_triangle(int size)
 	int i;
 	int j;
 	int k;
-
+	
+	if (size > 0)
+	{
 	for (i = 0; i < size; i++)
 	{
 	for (j = 0; j < size - i - 1; j++)
@@ -22,4 +24,10 @@ void print_triangle(int size)
 	}
 	_putchar('\n');
 	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+
 }
