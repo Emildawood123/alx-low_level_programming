90d8067b049f76d614d737f2ea9158adb5d363f4
Your Name <you@example.com>
2023-03-24 07:18:17 -0700

    traingle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - that print hashes as a triangle
+ * @size: times of hashes --
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size - i; j++)
+	{
+	_putchar(' ');
+	}
+	for (k = 0; k <= i; k++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+}
