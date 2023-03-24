d90a4b7c645d71c9f01081f48773e3323a7cf020
Your Name <you@example.com>
2023-03-24 06:40:03 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - that program print fizz if /3 buzz /5 fizzbuzz /3 and /5 else num
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else
+	{
+	printf("%d", i);
+	}
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
