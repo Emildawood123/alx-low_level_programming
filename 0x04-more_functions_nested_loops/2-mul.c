568c79cb56d5ac040ad7ad66963019f5eb921d7b
Your Name <you@example.com>
2023-03-24 04:47:44 -0700

    multiplaction two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - that return multiplication 2 numbers
+ * @a: that first number
+ * @b: that second number
+ * Return: multiblication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
