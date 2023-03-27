4f327fce5c4936cf75780ee9c15e069b7618bff0
Your Name <you@example.com>
2023-03-27 15:21:26 -0700

    second string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - that prints second half of the sentence
+ * @str: this is a sentence
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int beg;
+
+	while (str[len])
+	{
+	len++;
+	}
+	for (beg = len % 2 == 0 ? len / 2 : len + 1 / 2; beg <= len; beg++)
+	{
+	_putchar(str[beg]);
+	}
+	_putchar('\n');
+}
