8506cb2061dbddb161b5840556418cc8f5e1f5c8
Your Name <you@example.com>
2023-03-27 07:19:46 -0700

    swap two vars

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - that swap two variables
+ * @a: that first var
+ * @b: that second var
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+	m = *a;
+	*a = *b;
+	*b = m;
+}
