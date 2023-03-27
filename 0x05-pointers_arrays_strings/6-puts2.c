dfb3585d0cc631429a328b3a99bac3b1383213ee
Your Name <you@example.com>
2023-03-27 10:06:23 -0700

    only even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - that program prints even numbers
+ * @str: that are collections of numbers
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+	{
+	if (str[len] % 2 == 0)
+	{
+	_putchar(str[len]);
+	len++;
+	}
+	else
+	{
+	len++;
+	}
+	}
+	_putchar('\n');
+}
