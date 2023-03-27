41eea23e9dd0de84c17f0b4f430cd83dd6560c11
Your Name <you@example.com>
2023-03-27 07:50:45 -0700

    print by _putchar function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - that print withot printf
+ * @str: this is a string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+	_putchar(*str);
+	*str++;
+	}
+}
