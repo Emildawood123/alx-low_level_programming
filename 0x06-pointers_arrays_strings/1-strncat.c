60085b5a460398599959ae4445f7ffe4580a6feb
Your Name <you@example.com>
2023-03-29 06:21:47 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strncat - that return and change first parameter
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: numbers of token
+ * Return: new dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1 = 0;
+	int len2 = 0;
+	int i;
+
+	while (dest[len1])
+	{
+	len1++;
+	}
+	while (src[len2])
+	{
+	len2++;
+	}
+	if (n > len2)
+	{
+	for (i = 0; i <= len2; i++)
+	{
+	dest[len1 + i] = src[i];
+	}		
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	dest[len1 + i] = src[i];
+	}
+	}
+	
+	return(dest);
+}
