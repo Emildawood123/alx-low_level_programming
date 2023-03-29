503e2b7377e730223c5ae65becaf18bf6761976d
Your Name <you@example.com>
2023-03-29 09:04:03 -0700

    convert from small to capital

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - that change the string to upper
+ * @a: that is string
+ * Return: string(upper)
+ */
+char *string_toupper(char *a)
+{
+	int len = 0;
+	int i;
+
+	while (a[len])
+	{
+	len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+	if (i >= 97 && i <= 122)
+	{
+	a[i] = a[i] - 32;
+	}
+	}
+	return (a);
+}
