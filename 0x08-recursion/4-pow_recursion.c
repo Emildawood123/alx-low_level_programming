cac481ea44af5582019acb854ef4e6cfb8bdce66
Your Name <you@example.com>
2023-04-05 15:30:51 -0700

    princes

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - that program return x**y
+ * @x: that is number
+ * @y: this is power
+ * Return: x**y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+	return (-1);
+	}
+	else if (y == 0)
+	{
+	return (1);
+	}
+	else
+	{
+	return (x * _pow_recursion(x, y - 1));
+	}
+	return (1);
+}
