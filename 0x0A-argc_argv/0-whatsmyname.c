a7f3d25785095918895d7be49239a71ae124f532
Your Name <you@example.com>
2023-04-08 01:40:27 -0700

    filename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  program use argc and v to return filename
+ * @argc: that is int
+ * @argv: that is char
+ * Return: file name
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
