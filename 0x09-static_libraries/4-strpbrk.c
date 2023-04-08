5762a16d4312ba4b8fea94a06daeadd0936fa3e2
Your Name <you@example.com>
2023-04-07 20:56:21 -0700

    exa

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strpbrk - that is program temp else
+ * @s: para
+ * @accept: para
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr - that program temp
+ * @haystack: para
+ * @needle: para
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
