2b469e66a7803515a171efc11f28af2caa726718
Your Name <you@example.com>
2023-04-08 02:00:01 -0700

    nums of word

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - that program print sum of words
+ * @argc: that is int
+ * @argv: that is char
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 100)
+	{
+	printf("%s", argv[0]);
+	}
+	printf("%d\n", argc);
+	return (0);
+}
