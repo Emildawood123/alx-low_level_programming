a168072b366faa89b99c3c33192d2e4ea4c7d241
Your Name <you@example.com>
2023-04-11 18:27:52 -0700

    nice pic

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - that app return string
+ * @str: string
+ * Return: new string
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *nestr;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+	i++;
+	}
+	nestr = (char *)malloc(i);
+	while (i != 0)
+	{
+	nestr[i - 1] = str[i - 1];
+	i--;
+	}
+	if (nestr == NULL)
+	{
+	return (NULL);
+	}
+	return (nestr);
+}
