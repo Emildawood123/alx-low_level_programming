a071847bfb0bfd27ef798547fcc9496c991b83b5
Your Name <you@example.com>
2023-04-13 02:27:19 -0700

    okey agian

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * malloc_checked - taht program return nothi
+ * @b: num
+ * Return: nothing
+ */
 void *malloc_checked(unsigned int b)
 {
-	void *str; 
-	str = malloc(b);
+	void *str;
 
+	str = malloc(b);
 	if (str == NULL)
 	{
 	exit(98);
