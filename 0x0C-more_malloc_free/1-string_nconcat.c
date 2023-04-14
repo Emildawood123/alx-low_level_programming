aeddd812d725ccf58061f164a0926a422d368449
Your Name <you@example.com>
2023-04-14 00:58:28 -0700

    trin

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,9 +30,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (constr == NULL)
 		return (NULL);
 	for (i = 0; i < l ; i++)
-		if (i <= len1)
+		if (i < len1)
 			constr[i] = s1[i];
-		else if (i >= len2)
+		else if (i >= len1)
 		{
 		constr[i] = s2[a];
 		a++;
