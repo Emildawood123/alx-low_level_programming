5daf64357d48c5b7cc0e51a989d41e0043cc628b
Your Name <you@example.com>
2023-04-14 02:01:56 -0700

    ok agian

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0)
 	{
 	free(ptr);
-	return(NULL);
+	return (NULL);
 	}
 	pointer = malloc(new_size);
 	if (pointer == NULL)
