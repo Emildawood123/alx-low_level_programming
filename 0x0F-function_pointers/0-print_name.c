8fe39aa0e8bbc064259c268e47e6571d586a6a5e
Your Name <you@example.com>
2023-04-19 02:22:14 -0700

    sss

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - print name deband on paras
+ * @name: nam
+ * @f: pointer of function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
