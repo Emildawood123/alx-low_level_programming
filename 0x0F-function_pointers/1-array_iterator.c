6a53aa9f8edeb2170bef1212b3fbafaa2805aada
Your Name <you@example.com>
2023-04-19 02:32:08 -0700

    array iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - taht print ele as a it or hex
+ * @array: this is array
+ * @size: that is length
+ * @action: pointer of function
+ * Return:void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
