262ff67f063c67b9e61bbf408dfc972764cd94bd
Your Name <you@example.com>
2023-04-21 04:12:54 -0700

    nice

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - that program print number bet them sep
+ * @separator: separator
+ * @n: sum of numbers
+ * Result: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list(arg);
+va_start(arg, n);
+if (separator == NULL)
+{
+for (i = 0; i < n; i++)
+{
+printf("%d ", va_arg(arg, int));
+}
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+if (i == 0)
+{
+printf("%d", va_arg(arg, int));
+}
+else
+{
+printf("%s %d", separator, va_arg(arg, int));
+}
+}
+}
+printf("\n");
+}
