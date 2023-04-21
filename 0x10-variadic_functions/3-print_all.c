dfe4e86e5d19537323d2ba0178a6c20b253cc037
Your Name <you@example.com>
2023-04-21 04:56:34 -0700

    okey

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - that print all 
+ * @format: to access
+ * Return: void
+ */
+void print_all(const char * const format, ...);
