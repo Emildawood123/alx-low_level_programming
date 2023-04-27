b1d63ff3eeb49b8ee8a733e679512d521bf93a4e
Your Name <edawood997@gmail.com>
2023-04-27 04:30:13 -0700

    done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,15 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 /**
  * free_list - that function make list is freed
  * @head: this head
- * Return: void
  */
 void free_list(list_t *head)
 {
-list_t *temp;
 while (head)
 {
-temp = head->next;
 free(head);
 free(head->str);
-head = temp;
+head = head->next;
 }
 }
