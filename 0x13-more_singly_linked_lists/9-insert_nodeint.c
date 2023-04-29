e71b2d493ba960cef2094c035bab25d498697a96
Your Name <edawood997@gmail.com>
2023-04-29 12:50:18 -0700

    insert item

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - that insert item in list
+ * @head: head
+ * @idx: index is wanted to insert init
+ * @n: item->n
+ * Return: head
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+unsigned int i = 0;
+listint_t *item = *head;
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->n = n;
+
+while (i < idx - 1)
+{
+item = item->next;
+i++;
+}
+new->next = item->next;
+item->next = new;
+return (*head);
+}
