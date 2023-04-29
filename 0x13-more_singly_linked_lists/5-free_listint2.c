f3cd9abb6fb40bd0b9b2f2e2c07fcce440a0e6e9
Your Name <edawood997@gmail.com>
2023-04-29 03:07:40 -0700

    free sets null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint- that function return new node
+ * @head: head
+ * Return: new
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *h;
+while (*head != NULL)
+{
+h = (*head)->next;
+free(*head);
+*head = h;
+}
+}
