cfd724cdfcb3d44da9721cc2554a5afaf48b3d3e
Your Name <edawood997@gmail.com>
2023-05-04 05:22:17 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,14 +9,17 @@ unsigned int binary_to_uint(const char *b)
 {
 int i = 0;
 unsigned int res = 0;
-
-while (b[i])
-{
-if (b[i] < 48 || b[i] > 49)
+if (!b)
 {
 return (0);
 }
-res = 2 * res + (b[i] - '0');
+while (b[i])
+{
+if (b[i] < '0' || b[i] > '1')
+{
+return (0);
+}
+res = res * 2 + (b[i] - '0');
 i++;
 }
 return (res);
