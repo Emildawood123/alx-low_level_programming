73519b226c05b37045bef54b7a26cd140b3a1d91
Your Name <edawood997@gmail.com>
2023-05-08 09:39:55 -0700

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,13 +10,10 @@
 int main(int argc, char *argv[])
 {
 char *buffer = malloc(sizeof(char) * 1024);
-ssize_t file_from, file_to;
-ssize_t  r;
-ssize_t w;
-ssize_t c;
+ssize_t file_from, file_to, r, w, c;
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
 exit(97);
 }
 file_from = open(argv[1], O_RDONLY);
@@ -24,7 +21,7 @@ r = read(file_from, buffer, 1024);
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (file_from == -1 || r == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
@@ -32,10 +29,13 @@ file_from = open(argv[1], O_RDONLY | O_WRONLY);
 w = write(file_to, buffer, r);
 if (w == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 free(buffer);
 exit(99);
 }
+r = read(file_from, buffer, 1024);
+file_to = open(argv[2], O_WRONLY | O_APPEND);
+free(buffer);
 c = close(file_from);
 if (c == -1)
 {
@@ -43,7 +43,6 @@ dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
 free(buffer);
 exit(100);
 }
-free(buffer);
 close(file_from);
 close(file_to);
 return (0);
