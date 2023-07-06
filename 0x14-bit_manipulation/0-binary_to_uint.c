e19e6bb33b614db2c032f4cd402f3e4d33b5ad87
emil dawood <edawood997@gmail.com>
2023-07-06 06:34:32 -0700

    first task done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,5 +3,30 @@
 #include "main.h"
 unsigned int binary_to_uint(const char *b)
 {
+int len = 0;
+int i = 0;
+int pow = 1;
+unsigned int dec = 0;
+if (b == NULL)
+{
 return (0);
+}
+while (b[len])
+{
+if (b[len] != '0' && b[len] != '1')
+{
+return (0);
+}
+len++;
+pow = pow * 2;
+}
+len--;
+pow = pow / 2;
+while (b[i])
+{
+dec = dec + ((b[i] - '0') * pow);
+pow = pow / 2;
+i++;
+}
+return (dec);
 }
