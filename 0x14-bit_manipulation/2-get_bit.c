8f0f036d14327d42a23aaac79ef75bf5a9c27f57
emil dawood <edawood997@gmail.com>
2023-07-07 10:03:03 -0700

    done task two

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,11 +4,12 @@
  * get_bit - that funcation return index of bina..
  * @n: binary number
  * @index: index
+ * Return: index of binary
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 int new;
-if (n == 0)
+if (index > 63)
 {
 return (-1);
 }
