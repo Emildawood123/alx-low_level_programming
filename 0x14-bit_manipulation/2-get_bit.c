ae836512950b4118edc2506ee05acf43cdb883a6
emil dawood <edawood997@gmail.com>
2023-07-07 09:58:30 -0700

    done task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
 /**
@@ -8,10 +7,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+int new;
 if (n == 0)
 {
 return (-1);
 }
-int new = (n >> (index)) & 1;
+new = (n >> (index)) & 1;
 return (new);
 }
