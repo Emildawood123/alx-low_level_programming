03d23998c95e0549f4208cd65e909d90e12b7a6c
emil dawood <edawood997@gmail.com>
2023-07-07 11:41:56 -0700

    done task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * flip_bits - that function return oppsite
+ * @n: number
+ * @m: sec num
+ * Return: ones in new number after xor
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int i;
+unsigned int count = 0;
+int new = n ^ m;
+for (i = 0; i <= 31; i++)
+{
+if (new & (1 << i))
+{
+count++;
+}
+}
+return (count);
+}
