eb2e502686828835a96220cd1150afcb821ca1a0
emil dawood <edawood997@gmail.com>
2023-07-08 09:02:22 -0700

    done

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ *get_endianness - that return 1 or 0
+ *Return: 1 or 0
+ */
 int get_endianness(void)
 {
-	unsigned int e = 2;
+	int e = 1;
 	char *ch;
 	int new;
 
