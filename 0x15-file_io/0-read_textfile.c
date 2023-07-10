aa23fd16e269d505dc179328dfcd9a69c56e2eac
emil dawood <edawood997@gmail.com>
2023-07-10 09:07:43 -0700

    simple edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,6 +5,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fp;
 char *s;
+size_t re;
+size_t wr;
 if (filename == NULL)
 {
 return (0);
@@ -15,12 +17,13 @@ if(fp == -1)
 return (0);
 }
 s = malloc(letters);
-letters = read(fp, s, letters);
-if ((size_t) write(1, s, letters) != letters)
+re = read(fp, s, letters);
+wr = write(1, s, re);
+if (wr != re)
 {
 return (0);
 }
 close(fp);
 free(s);
-return (letters);
+return (re);
 }
