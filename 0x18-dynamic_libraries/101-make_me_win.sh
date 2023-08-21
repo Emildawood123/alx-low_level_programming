cfbc7392f922795f6dc06abc63f0fec366afe2bd
emil dawood <edawood997@gmail.com>
2023-08-21 03:26:02 -0700

    done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp_test https://raw.githubusercontent.com/Emildawood123/alx-low_level_programming/master/0x18-dynamic_libraries/librand_override.so
+wget -P /tmp_test https://raw.githubusercontent.com/Emildawood123/alx-low_level_programming/master/0x18-dynamic_libraries/toLastTask.so
 export LD_PRELOAD=/tmp_test/toLastTask.so
