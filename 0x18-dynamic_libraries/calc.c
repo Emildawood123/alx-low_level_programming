53401c62740c287e233229e1fc9fdaecec813ccc
emil dawood <edawood997@gmail.com>
2023-08-21 02:47:56 -0700

    python call c functions add and sub and mod and div and mul

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,50 @@
+/**
+ * add - that add two numbers
+ * @a: first
+ * @b: second
+ * Return: sum
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * sub - that substract two numbers
+ * @a: first
+ * @b: second
+ * Return: sum
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * mul - that multi.. two numbers
+ * @a: first
+ * @b: second
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * div - that division two numbers
+ * @a: first
+ * @b: second
+ * Return: sum
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * mod - that remiund two numbers
+ * @a: first
+ * @b: second
+ * Return: sum
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}
