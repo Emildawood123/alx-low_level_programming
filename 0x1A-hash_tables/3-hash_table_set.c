4649195122c80463299bdf63003f933a7b0af0e4
Emil dawood <edawood997@gmail.com>
2023-09-08 21:57:25 +0300

    d

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,34 +12,48 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 hash_node_t *new;
-hash_node_t *head;
 unsigned long int ind = key_index((unsigned char *) key, ht->size);
 new = malloc(sizeof(hash_node_t));
 if (ht == NULL || key == NULL || strlen(key) == 0)
 {
 return (0);
 }
+new = add_node(&(ht->array[ind]), value, key);
 if (new == NULL)
 {
-free(new);
 return (0);
 }
-head = ht->array[ind];
-new = head;
+return (1);
+}
+/**
+ * add_node - that return new hash table aft
+ * @head: head
+ * @key: key
+ * @value: value
+ * Return: new
+*/
+hash_node_t *add_node(hash_node_t **head, const char *value, const char *key)
+{
+hash_node_t *new;
+new = *head;
 while (new)
 {
-if (strcmp(new->key, key) == 0)
+if (strcmp(key, new->key) == 0)
 {
 free(new->value);
 new->value = strdup(value);
-return (1);
+return (*head);
 }
 new = new->next;
 }
 new = malloc(sizeof(hash_node_t));
-new->value = strdup(value);
+if (new == NULL)
+{
+return (NULL);
+}
 new->key = strdup(key);
-new->next = head;
-head = new;
-return (1);
+new->value = strdup(value);
+new->next = *head;
+*head = new;
+return (*head);
 }
