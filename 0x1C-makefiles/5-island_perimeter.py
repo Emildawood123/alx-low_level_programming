ebf878f3bed3979edff09cd2c43905b6adf5db6f
Emil dawood <edawood997@gmail.com>
2023-10-25 12:55:53 +0300

    d

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""function to know area of island"""
+
+
+def island_perimeter(grid):
+    res = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            minus = 0
+            if grid[i][j] == 1:
+                if grid[i-1][j] == 1:
+                    minus += 1
+                if grid[i+1][j] == 1:
+                    minus += 1
+                if grid[i][j-1] == 1:
+                    minus += 1
+                if grid[i][j+1] == 1:
+                    minus += 1
+                res += 4 - minus
+                if (minus == 0):
+                    return res
+            else:
+                continue
+    return res
